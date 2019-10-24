8e68e0ac51900cc0985897ac66eaba9679e9361a
Avalon <972003502@qq.com>
2019-10-24 17:34:11 +0800

    c

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
   res.header('Access-Control-Allow-Methods', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Max-Age', '1000');
   next();
 });
 
