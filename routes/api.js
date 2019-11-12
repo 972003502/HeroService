55e884fa593b8a3e98a11cc3bfa0b8ae7dd42650
Avalon <972003502@qq.com>
2019-11-13 01:30:29 +0800

    ''

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongodb = require('../middleware/mongodb');
+const crypto = require('crypto');
 
 router.use('/', mongodb.dbOperate);
 
